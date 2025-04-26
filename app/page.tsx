e2fb311593907dd5b5001c6fe763fdfbecf383db
Donald Franks <donald.franks@reddit.com>
2025-04-26 15:23:54 -0700

    2025 Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,97 @@
-import Image from "next/image";
+import React from "react";
+import localFont from "next/font/local"
+const redditFont = localFont({
+  src: [
+    {
+      path: '../public/fonts/RedditSans.ttf',
+      weight: '800'
+    },
+  ],
+});
 
-export default function Home() {
+export default function Go() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col lg:flex-row items-center max-h-screen min-h-screen p-24">
+      <div className="flex-col items-center text-center lg:text-left text-sm top-0 z-10 h-full w-full max-w-5xl lg:flex">
+        <div>
+          <p className="font-bold left-0 mx-auto text-5xl text-slate-200 top-0 tracking-tight w-full lg:pt-8 lg:static lg:rounded-xl">
+            Donald R. Franks
+          </p>
+          <p className="font-medium text-center lg:text-left left-0 mx-auto text-xl text-slate-400 tracking-tight top-0 w-full lg:static lg:pt-0 lg:rounded-xl">
+            Senior Software Engineer @ <span className={`${redditFont.className} text-[#FF4500] ml-1`}>reddit</span>
+          </p>
+          <p className="font-light text-center text-xs sm:text-sm lg:text-left mt-4 left-0 mx-auto text-slate-400 tracking-normal top-0 w-full lg:static lg:pt-0 lg:rounded-xl">
+            I build intuitive UIs, robust backend systems, and scalable APIs.
+          </p>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex flex-col gap-12 max-h-screen overflow-scroll h-full lg:mb-0 lg:w-full lg:max-w-5xl lg:text-left">
+        <span
+          className="group rounded-lg pl-4 border border-transparent py-4 transition-colors"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <h2 className="items-center flex flex-row justify-between mb-0 text-md md:text-lg lg:w-9/12 font-semibold">
+            <span>
+              Reddit <span className="text-sm">|</span> Senior Software Engineer
+            </span>
+            <span className="text-right text-sm">
+              2021 - Present
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+          <p className="m-0 mt-1 lg:w-3/4 text-sm text-slate-400">
+            At Reddit, I work on the Experimentation Team, where we develop a platform that empowers teams to seamlessly create, manage, and analyze experiments. I had the privilege of building the Experiments API, a gRPC service that streamlines the automation of experiment creation and management across services.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <p className="m-0 mt-4 lg:w-3/4 text-sm text-slate-400">
+          From implementing Mutually Exclusive Groups to redesigning our Experimentation UI, to overhauling our backend architecture, I have been fortunate to play a role in shaping the future of our platform.
+          </p>
+          <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">TypeScript</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">Lit-Element</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">Python</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">gRPC</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">Postgres</div>
+            </li>
+          </ul>
+        </span>
+        <span
+          className="group rounded-lg pl-4 border border-transparent py-4 transition-colors"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <h2 className="items-center flex flex-row justify-between mb-0 text-md md:text-lg lg:w-9/12 font-semibold">
+            <span>
+              Apple Inc. <span className="text-sm">|</span> Frontend Developer
+            </span>
+            <span className="text-right text-sm">
+              2018 - 2021
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="m-0 mt-1 lg:w-3/4 text-sm text-slate-400">
+            Complex, customized, feature-rich dashboards were my bread and butter at Apple. I utilized D3.js to track and visualize the progress and regressions for upcoming releases.
+            With over 100 dashboards tailored to various teams across Software Engineering, I was able to negotiate with sponsors, stakeholders, and managers to implement feedback and integrate enhancements.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">React</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">FastAPI</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">Jenkins</div>
+            </li>
+            <li className="mr-1.5 mt-2">
+              <div className="flex items-center rounded-md bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-400">GraphQL</div>
+            </li>
+          </ul>
+        </span>
       </div>
     </main>
   );
